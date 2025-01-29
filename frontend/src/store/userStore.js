a15e367bb4f6231f9543eb6ee7d32198d48d310b
NeerajGitHb <neerajvishwakarma726689@gmail.com>
2025-01-29 20:19:46 +0530

    local

--- frontend/src/store/userStore.js
+++ frontend/src/store/userStore.js
@@ -1,18 +1,18 @@
-import { create } from "zustand";
-import { persist } from "zustand/middleware";
+import { create } from 'zustand'
+import {persist} from 'zustand/middleware'
 
 const userStore = create(
-  persist(
-    (set) => ({
-      user: null,
-      setUser: (userData) => set({ user: userData }),
-      clearUser: () => set({ user: null }),
-    }),
-    {
-      name: "user-storage",
-      storage: localStorage, // ✅ Use `storage` instead of `getStorage`
-    }
-  )
-);
+    persist(
+        (set) =>({
+            user:null,
+            setUser:( userData) => set({user:userData}),
+            clearUser : () => set({user:null})
+        }),
+        {
+            name: "user-storage",
+            getStorage: () => localStorage,
+        }
+    )
+)
 
 export default userStore;
