b72d7e7570169fe418b28ab2dcf0ced0399bf822
NeerajGitHb <neerajvishwakarma726689@gmail.com>
2025-01-29 20:18:01 +0530

    no window

--- frontend/src/service/url.service.js
+++ frontend/src/service/url.service.js
@@ -4,24 +4,10 @@ const ApiUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
 
 const axiosInstance = axios.create({
     baseURL: ApiUrl,
-    withCredentials: true,  // Ensures cookies/auth tokens are sent with requests
+    withCredentials: true,
+    headers: {
+        "Content-Type": "application/json", // Ensures correct request type
+      },
 });
-
-// Request Interceptor to add the Authorization header to each request
-axiosInstance.interceptors.request.use(
-  (config) => {
-    // Check if we're on the client-side
-    if (typeof window !== "undefined") {
-      const token = localStorage.getItem("token");  // Get the token from localStorage
-      if (token) {
-        config.headers.Authorization = `Bearer ${token}`;  // Add token to Authorization header
-      }
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
 
 export default axiosInstance;
